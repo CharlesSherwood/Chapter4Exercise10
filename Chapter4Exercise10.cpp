3d97b62b92eb9d58c9a2ec636d1222ab375abef2
Charles Sherwood <kulan876321@yahoo.com>
2025-02-26 18:58:23 -0500

    Add project files.

---
+++ Chapter4Exercise10.cpp
@@ -0,0 +1,24 @@
+/*FileName:Chapter4Exercis10.cpp
+Progrmmer:Charles Sherwood
+Date:2/25
+Requirments:Ask the user for a day and month 
+and tell the mhow many days are the that
+month from that year.
+*/
+
+
+
+
+#include<iomanip>
+#include<cmath>
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
