33e7d9fdf5029c58442c29a24b08e4d856c05c7d
Charles Sherwood <kulan876321@yahoo.com>
2025-02-26 19:24:07 -0500

    Finished

--- Chapter4Exercise10.cpp
+++ Chapter4Exercise10.cpp
@@ -14,11 +14,37 @@ month from that year.
 #include <iostream>
 using namespace std;
 
-
-
-
 int main()
 {
-    std::cout << "Hello World!\n";
+  
+	int month, year;
+	//Get User Input
+	cout << "Please Enter A Month:";
+	cin >> month;
+	cout << "Please Enter A Year:";
+	cin >> year;
+
+	//Cheack if It would be a leap year or not, As well as checking other months with days
+	if ((month == 2) && ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)))) 
+		cout << "Number of days is 29";
+	
+		else if (month == 2) 
+			cout << "Number of days is 28";
+		
+			else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) 
+				cout << "Number of days is 31";
+	
+				else if (month == 4 || month == 6 || month == 9 || month == 11) 
+					cout << "Number of days is 30";
+				//IF none of those have been inputed correctly you have and invalid month
+					else cout << "You Have Entered An Invalid Month";
+
+	return 0;
+	
+	
+	
+	
+	
+	
 }
 
